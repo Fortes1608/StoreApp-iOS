b8ec5bf4383d254f04cf61f173528afe235f0ff9
Lorenzo Fortes <mrlorenzo1608@gmail.com>
2025-08-13 15:33:35 -0300

    Initial Commit

---
+++ APIActivity/APIActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIActivityApp.swift
+//  APIActivity
+//
+//  Created by Lorenzo Fortes on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct APIActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
